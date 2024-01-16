83bf89463d82f464acf7ebdf3fc56e3b3464a041
surajbhan-3 <surajbhan2boaz@gmail.com>
2024-01-16 20:53:52 +0530

    Minor changes

--- frontend/src/Context/AuthContext.jsx
+++ frontend/src/Context/AuthContext.jsx
@@ -213,7 +213,7 @@ export const AuthContextProvider = ({children})=>{
                           const userId = localStorage.getItem("userId")
                           console.log(userId, "userId")
                           console.log(localStorage.getItem("shopin-token"))
-                          const response = await axios.get(`${AUTH_BASE_URL}/products/cart/get_cartdata/${userId}`,
+                          const response = await axios.get(`${AUTH_BASE_URL}/api/products/cart/get_cartdata/${userId}`,
                           {
 
                             headers: {
